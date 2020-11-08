f8996e4eeca0640dfe0053de7cc7f47954c191bb
Lucas Tidas <Lucas@pc-225.home>
2020-11-08 13:37:48 +0100

    comments added

--- routes/task.js
+++ routes/task.js
@@ -41,13 +41,15 @@ router.get("/task/delete/:id", async (req, res) => {
     const { id } = req.params;
 
     const deleteTask = await Task.findById(id);
-    await deleteTask.deleteOne();
+    await deleteTask.deleteOne(); // suppression de la tâche => OK
 
-    const tasks = await Task.find();
+    const tasks = await Task.find(); // je récupère toutes les tâches
     tasks.map((task, index) => {
+      // puis je boucle pour leur réassigner une position qui me sert à les replacer correctement lorsque je les coche/décoche
       task.position = index;
     });
-    await tasks.save();
+
+    await tasks.save(); // le save() ne fonctionne pas, il me renvoi l'erreur => save is not a function
 
     res.status(200).json(tasks);
   } catch (err) {
@@ -58,6 +60,7 @@ router.get("/task/delete/:id", async (req, res) => {
 router.get("/tasks", async (req, res) => {
   try {
     const tasks = await Task.find();
+
     res.status(200).json(tasks);
   } catch (err) {
     res.status(400).json({ error: err.message });
